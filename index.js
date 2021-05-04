fc11fb8b4fcc9cf5bf8338feb7aef601746d9760
tati89 <malena.one@gmail.com>
2021-05-04 16:58:23 +0100

    user prompt created

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ function randomNumbers() {
 }
 
 function readyToPlay() {
-  alert("Ready to play?");
+  alert("Ready? Press enter and type the first number in 15 seconds");
+}
+
+function userPromptNumbers() {
+  answerNumbers = [];
+  for (let i = 1; i <= 4; i++) {
+    number = Number(prompt(`Please enter ${i} number:`));
+    answerNumbers.push(number);
+  }
+  return answerNumbers;
 }
 
 randomNumbers();
-let timeoutId = setTimeout(readyToPlay, 15000);
-clearTimeout(timeoutId);
+setTimeout(readyToPlay, 2000);
+setTimeout(userPromptNumbers, 15000);
